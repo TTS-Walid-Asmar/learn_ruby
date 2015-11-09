ba094e30c2dc443c047f1158779bbfdec8c6fd7c
rym465 <reneeymatthews@gmail.com>
2015-11-08 20:03:29 -0500

    Team Dylan and Renee's changes

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,28 @@
+def add (num_1, num_2)
+ num_1 + num_2
+end
+
+def subtract (num_1, num_2)
+ num_1 - num_2
+end
+
+def sum(array)
+   total = 0
+   array.each do | x |
+      total += x
+end
+   return total
+end
+
+def multiply (num_1, num_2)
+ result = num_1 * num_2
+ return result
+end
+
+def factorial 
+   total = 0
+   array.each do | x |
+      total -= x
+end
+   return total
+end
