3f6d8339e3635e59a6665dd4fc3691eb1e8fb04c
Shane Barringer <shane.barringer@outlook.com>
2015-11-08 22:17:27 -0500

    adds methods

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,3 +1,21 @@
-def simon_says
-puts "Hello"
-end
+# def echo(word)
+#   "#{word}"
+# end
+#
+# def shout(word)
+#   "#{word.upcase}"
+# end
+#
+# def repeat(words,n=2)
+#   "#{([words] * n).join(' ')}"
+# end
+#
+#
+# def start_of_word(word, number)
+#   "#{word[0..number-1]}"
+# end
+#
+# def first_word(words)
+#   word_array = words.split(" ")
+#   "#{word_array[0]}"
+# end
